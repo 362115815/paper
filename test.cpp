8924bd3d64ef179de7d404f7591db7581b1c1aa0
xiaominchen <xiaomin_chen@outlook.com>
2017-06-22 15:38:55 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: xiaominchen
+	> Mail: xiaomin_chen1993@outlook.com
+	> Created Time: Thu 22 Jun 2017 03:35:32 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	return 0;
+}
